97db347dc2b78e6ce43c5f48c381123d27bdd278
kiranbir singh <kiranbirsingh104@gmail.com>
2026-04-17 17:11:46 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,12 @@
 from model import train_model
+import joblib
 
-train_model()
+# Get all outputs
+model, sc, ct = train_model()
+
+# Save model
+model.save("model.h5")
+
+# Save preprocessing objects
+joblib.dump(sc, "scaler.pkl")
+joblib.dump(ct, "ct.pkl")
